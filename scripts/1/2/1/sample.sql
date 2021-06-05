6dbf8ce10a2a24d355decf3a30e0ad2d39cddb72
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-05 17:55:35 +0900

    Change fomatter

--- scripts/1/2/1/sample.sql
+++ scripts/1/2/1/sample.sql
@@ -1,24 +1,22 @@
-DROP TABLE corporation;
-
+DROP TABLE IF EXISTS corporation;
 CREATE TABLE corporation (
-	corp_id SMALLINT,
-	name VARCHAR(30),
-	CONSTRAINT pk_corporation PRIMARY KEY (corp_id)
-	);
-
-INSERT INTO corporation (
-	corp_id,
-	name
-	)
-VALUES (
-	27,
-	'ABC.COM'
-	);
-
-SELECT name
-FROM corporation
-WHERE corp_id = 27;
-
-SELECT name
-FROM corporation
-WHERE corp_id = 0;
+  corp_id SMALLINT,
+  name VARCHAR(30),
+  CONSTRAINT pk_corporation PRIMARY KEY (corp_id)
+);
+INSERT INTO
+  corporation (corp_id, name)
+VALUES
+  (27, 'ABC.COM');
+SELECT
+  name
+FROM
+  corporation
+WHERE
+  corp_id = 27;
+SELECT
+  name
+FROM
+  corporation
+WHERE
+  corp_id = 0;
