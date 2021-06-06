3ca8b178546e1b417c7baa3a6ebfad13ad5d39c5
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-06 22:46:46 +0900

    5.6.2

---
+++ scripts/5/practices/2.sql
@@ -0,0 +1,10 @@
+SELECT
+  c.cust_id,
+  c.fed_id,
+  p.name
+FROM
+  customer c
+  INNER JOIN account a ON c.cust_id = a.cust_id
+  INNER JOIN product p ON a.product_cd = p.product_cd
+WHERE
+  cust_type_cd = 'I';
