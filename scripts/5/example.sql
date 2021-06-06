fb9783b5ce4191058127dc8d0278308efc59be89
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-06 21:59:45 +0900

    5.1.2

--- scripts/5/example.sql
+++ scripts/5/example.sql
@@ -7,3 +7,17 @@ SELECT
 FROM
   employee e
   JOIN department d;
+SELECT
+  e.fname,
+  e.lname,
+  d.name
+FROM
+  employee e
+  INNER JOIN department d on e.dept_id = d.dept_id;
+SELECT
+  e.fname,
+  e.lname,
+  d.name
+FROM
+  employee e
+  INNER JOIN department d USING(dept_id);
