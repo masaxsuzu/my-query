728b2be0f9a3ac9af4b5d9ac593eadd59dd9b4f3
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-07 16:15:50 +0900

    10.1.2

--- scripts/10/example.sql
+++ scripts/10/example.sql
@@ -25,3 +25,29 @@ SELECT
 FROM
   account a
   RIGHT OUTER JOIN business b on a.cust_id = b.cust_id;
+SELECT
+  a.account_id,
+  a.product_cd,
+  CONCAT(i.fname, ' ', i.lname) person_name,
+  b.name business_name
+FROM
+  account a
+  LEFT OUTER JOIN individual i on a.cust_id = i.cust_id
+  LEFT OUTER JOIN business b on a.cust_id = b.cust_id;
+SELECT
+  a_i.account_id,
+  a_i.product_cd,
+  a_i.person_name,
+  b.name business_name
+FROM
+  (
+    SELECT
+      a.account_id,
+      a.product_cd,
+      a.cust_id,
+      CONCAT(i.fname, ' ', i.lname) person_name
+    FROM
+      account a
+      LEFT JOIN individual i on a.cust_id = i.cust_id
+  ) a_i
+  LEFT OUTER JOIN business b on a_i.cust_id = b.cust_id
