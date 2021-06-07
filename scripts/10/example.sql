a223a1c954586a7cdaddd8df4ae15a2268ef0b76
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-07 16:06:51 +0900

    10.1

---
+++ scripts/10/example.sql
@@ -0,0 +1,20 @@
+SELECT
+  a.account_id,
+  c.cust_id
+FROM
+  account a
+  INNER JOIN customer c on a.cust_id = c.cust_id;
+SELECT
+  a.account_id,
+  b.cust_id,
+  b.name
+FROM
+  account a
+  INNER JOIN business b on a.cust_id = b.cust_id;
+SELECT
+  a.account_id,
+  a.cust_id,
+  b.name
+FROM
+  account a
+  LEFT OUTER JOIN business b on a.cust_id = b.cust_id;
