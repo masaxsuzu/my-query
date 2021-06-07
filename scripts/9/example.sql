fddfeba52b586142aaa7c4bd81f25a6658da3a3a
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-07 14:12:36 +0900

    9.3.1

--- scripts/9/example.sql
+++ scripts/9/example.sql
@@ -30,4 +30,68 @@ WHERE
       e.title = 'Head Teller'
       AND b.city = 'Woburn'
   );
-
+SELECT
+  emp_id,
+  fname,
+  lname title
+FROM
+  employee
+WHERE
+  emp_id IN (
+    SELECT
+      superior_emp_id
+    FROM
+      employee
+  );
+SELECT
+  emp_id,
+  fname,
+  lname,
+  title
+FROM
+  employee
+WHERE
+  emp_id <> ALL (
+    SELECT
+      superior_emp_id
+    FROM
+      employee
+    WHERE
+      superior_emp_id IS NOT NULL
+  );
+SELECT
+  account_id,
+  cust_id,
+  product_cd,
+  avail_balance
+FROM
+  account
+WHERE
+  avail_balance < ALL (
+    SELECT
+      a.avail_balance
+    FROM
+      account a
+      INNER JOIN individual i ON a.cust_id = i.cust_id
+    WHERE
+      i.fname = 'Frank'
+      AND i.lname = 'Tucker'
+  );
+SELECT
+  account_id,
+  cust_id,
+  product_cd,
+  avail_balance
+FROM
+  account
+WHERE
+  avail_balance > ANY (
+    SELECT
+      a.avail_balance
+    FROM
+      account a
+      INNER JOIN individual i ON a.cust_id = i.cust_id
+    WHERE
+      i.fname = 'Frank'
+      AND i.lname = 'Tucker'
+  );
