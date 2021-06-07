6124f826c803283e6d444a3495e6bcc2c972e073
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-07 16:26:17 +0900

    10.2

--- scripts/10/example.sql
+++ scripts/10/example.sql
@@ -67,3 +67,101 @@ SELECT
 FROM
   employee e
   RIGHT OUTER JOIN employee e_mgr ON e.superior_emp_id = e_mgr.emp_id;
+SELECT
+  d1.x + d2.x + d3.x
+FROM
+  (
+    SELECT
+      0 x
+    UNION ALL
+    SELECT
+      1 x
+    UNION ALL
+    SELECT
+      2 x
+    UNION ALL
+    SELECT
+      3 x
+    UNION ALL
+    SELECT
+      4 x
+    UNION ALL
+    SELECT
+      5 x
+    UNION ALL
+    SELECT
+      6 x
+    UNION ALL
+    SELECT
+      7 x
+    UNION ALL
+    SELECT
+      8 x
+    UNION ALL
+    SELECT
+      9 x
+  ) d1
+  CROSS JOIN (
+    SELECT
+      0 x
+    UNION ALL
+    SELECT
+      10 x
+    UNION ALL
+    SELECT
+      20 x
+    UNION ALL
+    SELECT
+      30 x
+    UNION ALL
+    SELECT
+      40 x
+    UNION ALL
+    SELECT
+      50 x
+    UNION ALL
+    SELECT
+      60 x
+    UNION ALL
+    SELECT
+      70 x
+    UNION ALL
+    SELECT
+      80 x
+    UNION ALL
+    SELECT
+      90 x
+  ) d2
+  CROSS JOIN (
+    SELECT
+      0 x
+    UNION ALL
+    SELECT
+      100 x
+    UNION ALL
+    SELECT
+      200 x
+    UNION ALL
+    SELECT
+      300 x
+    UNION ALL
+    SELECT
+      400 x
+    UNION ALL
+    SELECT
+      500 x
+    UNION ALL
+    SELECT
+      600 x
+    UNION ALL
+    SELECT
+      700 x
+    UNION ALL
+    SELECT
+      800 x
+    UNION ALL
+    SELECT
+      900 x
+  ) d3
+ORDER BY
+  d1.x + d2.x + d3.x;
