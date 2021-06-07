8fe93884024c4a5f5bc45c71347325b18cff7fd7
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-07 14:46:04 +0900

    9.7.3

---
+++ scripts/9/practices/3.sql
@@ -0,0 +1,24 @@
+SELECT
+  e.emp_id,
+  e.fname,
+  e.lname,
+  levels.name
+FROM
+  employee e
+  INNER JOIN (
+    SELECT
+      'trainee' name,
+      '2004-01-01' start_dt,
+      '2005-12-31' end_dt
+    UNION ALL
+    SELECT
+      'worker' name,
+      '2002-01-01' start_dt,
+      '2003-12-31' end_dt
+    UNION ALL
+    SELECT
+      'mentor' name,
+      '2000-01-01' start_dt,
+      '2001-12-31' end_dt
+  ) levels ON e.start_date BETWEEN levels.start_dt
+  AND levels.end_dt;
