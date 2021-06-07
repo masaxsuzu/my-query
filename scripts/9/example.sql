275a89a88941cde80c8ca58cccb1f7c9f97dac75
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-07 14:30:38 +0900

    9.5.1

--- scripts/9/example.sql
+++ scripts/9/example.sql
@@ -125,3 +125,18 @@ WHERE
     WHERE
       b.cust_id = a.cust_id
   );
+SELECT
+  d.dept_id,
+  d.name,
+  e_cnt.how_many num_employees
+FROM
+  department d
+  INNER JOIN (
+    SELECT
+      dept_id,
+      COUNT(*) how_many
+    FROM
+      employee
+    GROUP BY
+      dept_id
+  ) e_cnt ON d.dept_id = e_cnt.dept_id;
