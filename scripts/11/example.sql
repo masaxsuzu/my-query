0869db2b299c7bb34d6fede14c6755029a526d70
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-09 21:40:47 +0900

    11.2.1

--- scripts/11/example.sql
+++ scripts/11/example.sql
@@ -2,11 +2,23 @@ SELECT
   c.cust_id,
   c.fed_id,
   CASE
-    WHEN c.cust_type_cd = 'I' THEN CONCAT(i.fname, ' ', i.lname)
-    WHEN c.cust_type_cd = 'B' THEN b.name
+    WHEN c.cust_type_cd = 'I' THEN (
+      SELECT
+        CONCAT(i.fname, ' ', i.lname)
+      FROM
+        individual i
+      WHERE
+        c.cust_id = i.cust_id
+    )
+    WHEN c.cust_type_cd = 'B' THEN (
+      SELECT
+        b.name
+      FROM
+        business b
+      WHERE
+        c.cust_id = b.cust_id
+    )
     ELSE 'UNKNOWN'
   END name
 FROM
-  customer c
-  LEFT OUTER JOIN individual i on c.cust_id = i.cust_id
-  LEFT OUTER JOIN business b on c.cust_id = b.cust_id;
+  customer c;
