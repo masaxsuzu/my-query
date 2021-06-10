23ea2a7627b28181775b233b3d87aeb2809adef3
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-10 18:09:24 +0900

    13.1.3

--- scripts/13/example.sql
+++ scripts/13/example.sql
@@ -9,3 +9,31 @@ SELECT
   *
 FROM
   department;
+SELECT
+  *
+FROM
+  account;
+EXPLAIN
+SELECT
+  cust_id,
+  SUM(avail_balance) tol_bal
+FROM
+  account
+WHERE
+  cust_id in (1, 5, 9, 11)
+GROUP BY
+  cust_id \G;
+ALTER TABLE
+  account
+ADD
+  INDEX acc_bal_idx(cust_id, avail_balance);
+EXPLAIN
+SELECT
+  cust_id,
+  SUM(avail_balance) tol_bal
+FROM
+  account
+WHERE
+  cust_id in (1, 5, 9, 11)
+GROUP BY
+  cust_id \G;
